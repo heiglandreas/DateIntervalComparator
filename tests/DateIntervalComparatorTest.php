9719caa409c12754e5c13b13bef537008eb4983f
Andreas Heigl <andreas@heigl.org>
2026-07-26 15:43:51 +0200

    Run tests on GHA against all supported PHPs

--- tests/DateIntervalComparatorTest.php
+++ tests/DateIntervalComparatorTest.php
@@ -1,4 +1,7 @@
 <?php
+
+declare(strict_types=1);
+
 /**
  * Copyright (c) Andreas Heigl<andreas@heigl.org>
  * Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -27,102 +30,108 @@
 namespace Org_Heigl\DateIntervalComparatorTest;
 
 use DateInterval;
-use phpDocumentor\Reflection\Types\Void_;
-use PHPUnit\Framework\TestCase;
 use Org_Heigl\DateIntervalComparator\DateIntervalComparator;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\Attributes\Small;
+use PHPUnit\Framework\TestCase;
 use UnexpectedValueException;
 
+#[CoversClass(DateIntervalComparator::class)]
+#[Small]
 class DateIntervalComparatorTest extends TestCase
 {
-    public function testThatDifferentYearsResolveCorrect()
-    {
-        $comparator = new DateIntervalComparator();
-        $this->assertEquals(-1, $comparator->compare(new \DateInterval('P1Y'), new \DateInterval('P2Y')));
-        $this->assertEquals(1, $comparator->compare(new \DateInterval('P2Y'), new \DateInterval('P1Y')));
-        $this->assertEquals(0, $comparator->compare(new \DateInterval('P1Y'), new \DateInterval('P1Y')));
-    }
+	public function testThatDifferentYearsResolveCorrect()
+	{
+		$comparator = new DateIntervalComparator();
+		$this->assertEquals(-1, $comparator->compare(new \DateInterval('P1Y'), new \DateInterval('P2Y')));
+		$this->assertEquals(1, $comparator->compare(new \DateInterval('P2Y'), new \DateInterval('P1Y')));
+		$this->assertEquals(0, $comparator->compare(new \DateInterval('P1Y'), new \DateInterval('P1Y')));
+	}
 
-    public function testThatDifferentMonthsResolveCorrect()
-    {
-        $comparator = new DateIntervalComparator();
-        $this->assertEquals(-1, $comparator->compare(new \DateInterval('P1M'), new \DateInterval('P2M')));
-        $this->assertEquals(1, $comparator->compare(new \DateInterval('P2M'), new \DateInterval('P1M')));
-        $this->assertEquals(0, $comparator->compare(new \DateInterval('P1M'), new \DateInterval('P1M')));
-    }
+	public function testThatDifferentMonthsResolveCorrect()
+	{
+		$comparator = new DateIntervalComparator();
+		$this->assertEquals(-1, $comparator->compare(new \DateInterval('P1M'), new \DateInterval('P2M')));
+		$this->assertEquals(1, $comparator->compare(new \DateInterval('P2M'), new \DateInterval('P1M')));
+		$this->assertEquals(0, $comparator->compare(new \DateInterval('P1M'), new \DateInterval('P1M')));
+	}
 
-    public function testThatDifferentDaysResolveCorrect()
-    {
-        $comparator = new DateIntervalComparator();
-        $this->assertEquals(-1, $comparator->compare(new \DateInterval('P1D'), new \DateInterval('P2D')));
-        $this->assertEquals(1, $comparator->compare(new \DateInterval('P2D'), new \DateInterval('P1D')));
-        $this->assertEquals(0, $comparator->compare(new \DateInterval('P1D'), new \DateInterval('P1D')));
-    }
+	public function testThatDifferentDaysResolveCorrect()
+	{
+		$comparator = new DateIntervalComparator();
+		$this->assertEquals(-1, $comparator->compare(new \DateInterval('P1D'), new \DateInterval('P2D')));
+		$this->assertEquals(1, $comparator->compare(new \DateInterval('P2D'), new \DateInterval('P1D')));
+		$this->assertEquals(0, $comparator->compare(new \DateInterval('P1D'), new \DateInterval('P1D')));
+	}
 
-    public function testThatDifferentHoursResolveCorrect()
-    {
-        $comparator = new DateIntervalComparator();
-        $this->assertEquals(-1, $comparator->compare(new \DateInterval('PT1H'), new \DateInterval('PT2H')));
-        $this->assertEquals(1, $comparator->compare(new \DateInterval('PT2H'), new \DateInterval('PT1H')));
-        $this->assertEquals(0, $comparator->compare(new \DateInterval('PT1H'), new \DateInterval('PT1H')));
-    }
+	public function testThatDifferentHoursResolveCorrect()
+	{
+		$comparator = new DateIntervalComparator();
+		$this->assertEquals(-1, $comparator->compare(new \DateInterval('PT1H'), new \DateInterval('PT2H')));
+		$this->assertEquals(1, $comparator->compare(new \DateInterval('PT2H'), new \DateInterval('PT1H')));
+		$this->assertEquals(0, $comparator->compare(new \DateInterval('PT1H'), new \DateInterval('PT1H')));
+	}
 
-    public function testThatDifferentMinutesResolveCorrect()
-    {
-        $comparator = new DateIntervalComparator();
-        $this->assertEquals(-1, $comparator->compare(new \DateInterval('PT1M'), new \DateInterval('PT2M')));
-        $this->assertEquals(1, $comparator->compare(new \DateInterval('PT2M'), new \DateInterval('PT1M')));
-        $this->assertEquals(0, $comparator->compare(new \DateInterval('PT1M'), new \DateInterval('PT1M')));
-    }
+	public function testThatDifferentMinutesResolveCorrect()
+	{
+		$comparator = new DateIntervalComparator();
+		$this->assertEquals(-1, $comparator->compare(new \DateInterval('PT1M'), new \DateInterval('PT2M')));
+		$this->assertEquals(1, $comparator->compare(new \DateInterval('PT2M'), new \DateInterval('PT1M')));
+		$this->assertEquals(0, $comparator->compare(new \DateInterval('PT1M'), new \DateInterval('PT1M')));
+	}
 
-    public function testThatDifferentSecondsResolveCorrect()
-    {
-        $comparator = new DateIntervalComparator();
-        $this->assertEquals(-1, $comparator->compare(new \DateInterval('PT1S'), new \DateInterval('PT2S')));
-        $this->assertEquals(1, $comparator->compare(new \DateInterval('PT2S'), new \DateInterval('PT1S')));
-        $this->assertEquals(0, $comparator->compare(new \DateInterval('PT1S'), new \DateInterval('PT1S')));
-    }
+	public function testThatDifferentSecondsResolveCorrect()
+	{
+		$comparator = new DateIntervalComparator();
+		$this->assertEquals(-1, $comparator->compare(new \DateInterval('PT1S'), new \DateInterval('PT2S')));
+		$this->assertEquals(1, $comparator->compare(new \DateInterval('PT2S'), new \DateInterval('PT1S')));
+		$this->assertEquals(0, $comparator->compare(new \DateInterval('PT1S'), new \DateInterval('PT1S')));
+	}
 
-    /** @dataProvider dateDiffResolvesCorrectlyProvider */
-    public function testThatDateDiffResolvesCorrectly($interval1, $interval2, $expected)
-    {
-        $comparator = new DateIntervalComparator();
+	/** @dataProvider dateDiffResolvesCorrectlyProvider */
+	#[DataProvider('dateDiffResolvesCorrectlyProvider')]
+	public function testThatDateDiffResolvesCorrectly($interval1, $interval2, $expected)
+	{
+		$comparator = new DateIntervalComparator();
 
-        $this->assertEquals($expected, $comparator->compare($interval1, $interval2));
-    }
+		$this->assertEquals($expected, $comparator->compare($interval1, $interval2));
+	}
 
-    public function dateDiffResolvesCorrectlyProvider()
-    {
-        return [
-            [new \DateInterval('P1Y2M3DT11H12M23S'), new \DateInterval('P1Y2M3DT11H12M23S'), 0],
-            [new \DateInterval('P1Y2M2DT11H12M23S'), new \DateInterval('P1Y2M3DT11H12M23S'), -1],
-            [(new \DateTime('2016-02-28 12:00:00'))->diff(new \DateTime('2016-03-01 12:00:00')), new \DateInterval('P2D'), 0],
-            [(new \DateTime('2015-02-28 12:00:00'))->diff(new \DateTime('2015-03-01 12:00:00')), new \DateInterval('P2D'), -1],
-            [(new \DateTime('2015-02-28 12:00:00'))->diff(new \DateTime('2015-03-01 12:00:00')), new \DateInterval('P1D'), 0],
-        ];
-    }
+	public static function dateDiffResolvesCorrectlyProvider()
+	{
+		return [
+			[new \DateInterval('P1Y2M3DT11H12M23S'), new \DateInterval('P1Y2M3DT11H12M23S'), 0],
+			[new \DateInterval('P1Y2M2DT11H12M23S'), new \DateInterval('P1Y2M3DT11H12M23S'), -1],
+			[(new \DateTime('2016-02-28 12:00:00'))->diff(new \DateTime('2016-03-01 12:00:00')), new \DateInterval('P2D'), 0],
+			[(new \DateTime('2015-02-28 12:00:00'))->diff(new \DateTime('2015-03-01 12:00:00')), new \DateInterval('P2D'), -1],
+			[(new \DateTime('2015-02-28 12:00:00'))->diff(new \DateTime('2015-03-01 12:00:00')), new \DateInterval('P1D'), 0],
+		];
+	}
 
-    /** @dataProvider safeDiffThrower */
-    public function testThatSafeDiffThrowsOnInvalidData($first, $second): void
-    {
-        $comparator = new DateIntervalComparator();
-        $comparator->safe(true);
-        self::expectException(UnexpectedValueException::class);
-        $comparator->compare($first, $second);
-    }
+	/** @dataProvider safeDiffThrower */
+	#[DataProvider('safeDiffThrower')]
+	public function testThatSafeDiffThrowsOnInvalidData($first, $second): void
+	{
+		$comparator = new DateIntervalComparator();
+		$comparator->safe(true);
+		self::expectException(UnexpectedValueException::class);
+		$comparator->compare($first, $second);
+	}
 
-    public function safeDiffThrower(): array
-    {
-        return [
-            [new DateInterval('P12M'), new DateInterval('P13M')],
-            [new DateInterval('P13M'), new DateInterval('P12M')],
-            [new DateInterval('P32D'), new DateInterval('P31D')],
-            [new DateInterval('P31D'), new DateInterval('P32D')],
-            [new DateInterval('PT25H'), new DateInterval('PT26H')],
-            [new DateInterval('PT26H'), new DateInterval('PT25H')],
-            [new DateInterval('PT60M'), new DateInterval('PT61M')],
-            [new DateInterval('PT61M'), new DateInterval('PT60M')],
-            [new DateInterval('PT61S'), new DateInterval('PT62S')],
-            [new DateInterval('PT62S'), new DateInterval('PT61S')],
-        ];
-    }
+	public static function safeDiffThrower(): array
+	{
+		return [
+			[new DateInterval('P12M'), new DateInterval('P13M')],
+			[new DateInterval('P13M'), new DateInterval('P12M')],
+			[new DateInterval('P32D'), new DateInterval('P31D')],
+			[new DateInterval('P31D'), new DateInterval('P32D')],
+			[new DateInterval('PT25H'), new DateInterval('PT26H')],
+			[new DateInterval('PT26H'), new DateInterval('PT25H')],
+			[new DateInterval('PT60M'), new DateInterval('PT61M')],
+			[new DateInterval('PT61M'), new DateInterval('PT60M')],
+			[new DateInterval('PT61S'), new DateInterval('PT62S')],
+			[new DateInterval('PT62S'), new DateInterval('PT61S')],
+		];
+	}
 }
